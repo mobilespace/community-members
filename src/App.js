85f9c2ee01accd9e4ef55492910bfabe9838ec4e
Teodora <teodora@sfs-wifi-dhcp-10-143-151-92.wifi.sfsu.edu>
2018-02-04 14:19:02 -0800

    teodora portfolio added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles/App.css';
 
 // List of members
 const MONTE_AVATAR = 'https://avatars2.githubusercontent.com/u/7840686?s=400&v=4'
+const TEODORA_IMG ='https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/c44.0.320.320/p320x320/20914645_1579156895438534_1027285013181250707_n.jpg?oh=0ff1b3c4c8bc8e00edb84cecd660d0fe&oe=5B1C897A'
 
 export default class App extends Component {
   render() {
@@ -14,6 +15,9 @@ export default class App extends Component {
         <div className="landing-page-avatar-container">
           <Link to="/monte">
             <img className="landing-page-avatar" src={MONTE_AVATAR} />
+          </Link>
+          <Link to="/teodora">
+            <img className="landing-page-avatar" src={TEODORA_IMG} />
           </Link>
         </div>
       </div>
---
+++ src/members/TeodoraCaneva.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/MonteThakkar.css'
+
+import LeftArrow from '../assets/left-arrow.png';
+
+
+const TEODORA_IMG ='https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/c44.0.320.320/p320x320/20914645_1579156895438534_1027285013181250707_n.jpg?oh=0ff1b3c4c8bc8e00edb84cecd660d0fe&oe=5B1C897A'
+export default class TeodoraCaneva extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <Link to="/" className="back-arrow-container">
+          <img className="back-arrow-icon" src={LeftArrow} />
+        </Link>
+        <div className="avatar-container">
+          <img className="avatar" src={TEODORA_IMG} />
+        </div>
+        <div className="title">Teodora Caneva</div>
+        <div className="subtitle">Work in Progress</div>
+        <div className="links-container">
+          <a href="https://github.com/kodekat" className="link">
+            <img className="links-icon" src="http://bogas04.github.io/img/github.svg" />
+          </a>
+          <a href="https://twitter.com/" className="link">
+            <img className="links-icon" src="http://www.freeiconspng.com/uploads/twitter-icon-download-18.png" />
+          </a>
+          <a href="mailto:teddycaneva@gmail.com?Subject=I%20would%20like%20to%20know%20more%20about%20MobileSpace!" target="_top" className="link">
+            <img className="links-icon" src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png" />
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
