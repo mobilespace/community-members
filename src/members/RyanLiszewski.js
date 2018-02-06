36b00e02ddcbf495ef9b895b4b95ffabac7e5e9f
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-06 13:11:26 -0800

    Added coffee animation. Need to fix pouring animation and className

--- src/members/RyanLiszewski.js
+++ src/members/RyanLiszewski.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../styles/RyanLiszewski.css'
+import '../styles/CoffeeAnimation.css'
 
 import LeftArrow from '../assets/left-arrow.png';
 
@@ -19,8 +20,112 @@ export default class RyanLiszewski extends Component {
               <img className="avatar-ryan" src={RYAN_AVATAR} />
           </div>
         </div>
+
         <div className="title-ryan">Ryan Liszewski</div>
-        <div className="subtitle-ryan">Jerk of all trades.</div>
+        <div className="subtitle-ryan">Lover of coffee, coffee shops and more coffee.</div>
+
+        <div className = "animation-container">
+          <div class="coffee-bag-container">
+            <div class="coffee-bag">
+              <span class="mast"></span>
+              <span class="bag-top"></span>
+              <span class="bag-body"></span>
+              <span class="logo">
+                <span class="bean bean-one"></span>
+                <span class="bean bean-two"></span>
+              </span>
+              <span class="info info-one"></span>
+              <span class="info info-two"></span>
+              <span class="info info-three"></span>
+              <span class="bag-bottom"></span>
+              
+            </div> {/* ./coffee-bag */}
+          </div> {/* ./coffee-bag-container  */}
+          
+          <span class="grounds-container">
+          <span class="grounds grounds-one"></span>
+          <span class="grounds grounds-two"></span>
+          <span class="grounds grounds-three"></span>
+          <span class="grounds grounds-four"></span>
+          </span>
+        
+          <div class="filter"></div> 
+          <span class="roasting-top"></span>
+        
+          <div class="roasting-body">
+            <span class="handle-container">
+              <span class="handle-one"></span>
+              <span class="handle-two"></span>
+            </span>
+          </div>
+        
+        {/* <!-- the black part between the coffee pot and the filter --> */}
+          <span class="screen"></span>
+          
+          <span class="water-attachment"></span>
+          
+          <span class="water-lid"></span>
+          
+          <span class="water-pot">
+            <span class="water-pot-gloss"></span>
+            <span class="fill-line fill-line-first"></span>
+            <span class="fill-line fill-line-last"></span>
+            <span class="water"></span>
+          </span>
+        
+          <span class="warmer-plate"></span>
+        
+          <span class="warmer">
+            <span class="warmer-accent accent-one"></span>
+            <span class="warmer-accent accent-two"></span>
+            <span class="warmer-accent accent-three"></span>
+          </span>
+        
+          <span class="warmer-base"></span>
+        
+          <span class="coffee-pot-container">
+        
+          <span class="coffee-pot-top"></span>
+          
+          <span class="coffee-pot-glass-top"></span>
+          
+          <span class="coffee-pot-glass-connector"></span>
+          
+          <span class="coffee-pot-glass-base">
+            <span class="coffee-pot-gloss"></span>
+          </span>
+          
+          <span class="coffee-pot-handle-container">
+            <span class="handle-top"></span>
+            <span class="handle-bottom"></span>
+          </span>
+          
+          <span class="coffee-drip"></span>
+        
+          <span class="drip-cover"></span>
+          
+          <span class="coffee"></span>
+        
+          </span>
+          
+          <span class="coffee-warmer-plate"></span>
+          
+          <span class="coffee-base-left-end"></span>
+          
+          <span class="coffee-maker-base"></span>
+          
+          <span class="control-panel">
+            <span class="gauge-one"></span>
+            <span class="gauge-two"></span>
+          </span>
+          
+          <span class="coffee-base-right-end"></span>
+          
+          <span class="leg leg-one"></span>
+          
+          <span class="leg leg-two"></span>
+		  
+        </div> 
         <div className="links-container-ryan">
           <a href="https://github.com/ryanliszewski" className="link">
             <img className="links-icon" src="http://bogas04.github.io/img/github.svg" />
