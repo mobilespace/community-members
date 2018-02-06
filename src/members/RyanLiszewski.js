eae02c97c30d79d37c54912fabeabfcff21c2831
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-05 21:34:43 -0800

    Fixed global css error

--- src/members/RyanLiszewski.js
+++ src/members/RyanLiszewski.js
@@ -10,15 +10,15 @@ const RYAN_AVATAR = 'https://avatars3.githubusercontent.com/u/5275250?s=460&v=4'
 export default class RyanLiszewski extends Component {
   render() {
     return (
-      <div className="main-container">
+      <div className="main-container-ryan">
         <Link to="/" className="back-arrow-container">
           <img className="back-arrow-icon" src={LeftArrow} />
         </Link>
         <div className="avatar-container">
           <img className="avatar" src={RYAN_AVATAR} />
         </div>
-        <div className="title">Ryan Liszewski</div>
-        <div className="subtitle">Jerk of all trades.</div>
+        <div className="title-ryan">Ryan Liszewski</div>
+        <div className="subtitle-ryan">Jerk of all trades.</div>
         <div className="links-container">
           <a href="https://github.com/ryanliszewski" className="link">
             <img className="links-icon" src="http://bogas04.github.io/img/github.svg" />
