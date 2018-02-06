4dceb2627ea27d1dc806b1a619564b83ceca62d4
mitulsavani <mitul.savani2@yahoo.com>
2018-02-05 18:22:06 -0800

    Play around with css file.

--- src/members/MitulSavani.js
+++ src/members/MitulSavani.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import '../styles/MonteThakkar.css'
+import '../styles/MitulSavani.css'
 
 import LeftArrow from '../assets/left-arrow.png';
 
@@ -18,7 +18,7 @@ export default class MitulSavani extends Component {
           <img className="avatar" src={MITUL_AVATAR} />
         </div>
         <div className="title">Mitul Savani</div>
-        <div className="subtitle">In love with Java and Mathematics</div>
+        <div className="subtitle">In love with Java Mathematics</div>
         <div className="links-container">
           <a href="https://github.com/mitulsavani" className="link">
             <img className="links-icon" src="http://bogas04.github.io/img/github.svg" />
