e0d0df8ced40da1258da8cbc39c563da086ab616
mitulsavani <mitul.savani2@yahoo.com>
2018-02-08 01:22:28 -0800

    Hurray! 🎉 Changed fonts, font color, profile picture 😎 , and added Linkedin link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './styles/App.css';
 
 // List of members
 const MONTE_AVATAR = 'https://avatars2.githubusercontent.com/u/7840686?s=400&v=4'
-const MITUL_AVATAR = 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAqdAAAAJGYzNDZlZmE5LWUwYWEtNGVlMC1hODE5LTBmY2JkYTQ2ZDEwOA.jpg'
+const MITUL_AVA = 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAqdAAAAJGYzNDZlZmE5LWUwYWEtNGVlMC1hODE5LTBmY2JkYTQ2ZDEwOA.jpg'
+const MITUL_AVATAR = 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/18556546_1553987881278489_849408995015005895_o.jpg?oh=bb65219475a9cbac85ca7090e7ee877b&oe=5ADE398B'
 const RYAN_AVATAR = 'https://avatars3.githubusercontent.com/u/5275250?s=460&v=4'
 const THOMAS_AVATAR = 'http://thomaszhu.com/resources/thomas_avatar.jpeg'
 const BHAVESH_AVATAR = 'https://avatars3.githubusercontent.com/u/13439148?s=460&v=4'
