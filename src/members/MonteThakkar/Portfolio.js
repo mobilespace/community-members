364e0eee78563e59e05cfdd079ff6caa73257778
Monte Thakkar <manthan.thakkar@gmail.com>
2018-02-10 11:40:28 -0800

    added portfolio page with resume

--- src/members/MonteThakkar/Portfolio.js
+++ src/members/MonteThakkar/Portfolio.js
@@ -5,10 +5,23 @@ import Dock from './Dock';
 
 import '../../styles/MonteThakkar.css'
 
+import LinkIcon from '../../assets/link.png'
+import ResumeIcon from '../../assets/resume.png'
+
 export default class Portfolio extends Component {
   render() {
     return (
       <div className="main-container">
+        <div className="portfolio-container">
+          <div className="link-row">
+            <img className="link-icon" src={LinkIcon}></img>
+            <a className="link-title" href="https://montethakkar.now.sh/">montethakkar.com</a>
+          </div>
+          <div className="link-row">
+            <img className="link-icon" src={ResumeIcon}></img>
+            <Link to="/MonteThakkar_Resume.pdf" className="link-title" target="_blank">resume</Link>
+          </div>
+        </div>
         <Dock />
       </div>
     );
