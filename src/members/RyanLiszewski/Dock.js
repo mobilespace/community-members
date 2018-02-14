af4721d6ff7307d5bdfab019b5415db1a10bfea8
Ryanliszewski <ryanliszewski@gmail.com>
2018-02-14 15:31:43 -0800

    Started implmenting dock bar.

--- src/members/RyanLiszewski/Dock.js
+++ src/members/RyanLiszewski/Dock.js
@@ -3,11 +3,30 @@ import { Link } from 'react-router-dom';
 
 import '../../styles/RyanLiszewski.css';
 
+import HOME_ICON from '../../assets/home.png';
+
 export default class Dock extends Component {
 
   render() {
     return (
-      <div className = "dock-container-ryan"></div> 
+      <div className = "dock-container-ryan">
+        <div className = "navbar-container">
+          <ul className ="navbar">
+            <li className="navbar-item">
+              <Link to="/ryan/dashboard">
+                <img className="navbar-icon" src={HOME_ICON}/>
+              </Link>  
+            </li>
+            <li className="navbar-item">
+              <Link to="/ryan/dashboard">
+                <img className="navbar-icon" src={HOME_ICON}/>
+              </Link>  
+            </li>
+          </ul>
+        </div>
+      
+      </div> 
+
     );
   }
 }
