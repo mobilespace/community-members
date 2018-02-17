66dae537e1f7934df452631b3363c968fd288497
niszeto <niszeto@gmail.com>
2018-02-17 10:43:06 -0800

    added name in about page and changed some minor text

--- src/members/NicholasSzeto/Profile.js
+++ src/members/NicholasSzeto/Profile.js
@@ -17,6 +17,7 @@ export default class Profile extends Component {
       <div className="main-container-nicholas">
         <div className="About">
           <div className="AboutLeftContainer">
+          <div className="title-nicholas">Nicholas Szeto</div>
             <div className="ProfileImageContainer NoSelection">
               <img
                 className="ProfileImage"
@@ -45,7 +46,7 @@ export default class Profile extends Component {
             <p className="IntroHello">About me</p>
             <p className="AboutText"><span className="Bold AboutText Inline">tldr;</span> Aspring Programmer that works hard to become better everyday.</p>
             <p className="AboutText">-----</p>
-            <p className="AboutText">I was born in 1996 and raised in San Francisco, California. I never really liked school at all while growing up. I knew that it was best to do well and work hard which I did. I just couldn't wait until the day where I did not have to think about school. During my senior year of high school in my AP Calculus class, I learned that I do not like math but I do like the problem solving aspect of math. When I got to college I had no idea what I wanted to get into as I didn't like any of my classes, but I heard of computer programming from my brother. I always liked computers and technology as they were a mean to aid in people's lives. So I gave that a chance and I stuck with pursuing a computer science degree after realizing that I like problem solving and computers. I know that I am not the best programmer around, but I put in a lot of effort in my projects and try to get better.   
+            <p className="AboutText">I was born in 1996 and raised in San Francisco, California. I never really liked school at all while growing up. I just couldn't wait until the day where I did not have to think about school. During my senior year of high school in my AP Calculus class, I learned that I do not like math but, I do like the problem solving aspect of math. When I got to college I had no idea what I wanted to get into as I didn't like any of my classes, but I heard of computer programming from my brother. I always liked computers and technology as they were a mean to aid in people's lives. So I gave that a chance and I stuck with pursuing a computer science degree after realizing that I like problem solving and computers. I know that I am not the best programmer around, but I put in a lot of effort in my projects and try to get better.   
             </p>
             <p className="AboutText">
               Besides school and studying, in my free time I like to cook random food dishes that I find interesting, performing archery, and just having some time to do nothing. I feel like many of these activities doesn't require too much thinking after the initial thought and planning, so I believe that is why I enjoy these activities so much.
