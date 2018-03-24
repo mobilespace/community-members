277b9dddb005b26d6135b69305a7acea6207e08f
Monte Thakkar <manthan.thakkar@gmail.com>
2018-03-24 16:58:19 -0700

    updated; looks good

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ const KARAN_AVATAR = 'https://avatars1.githubusercontent.com/u/19352928?s=460&v=
 export default class App extends Component {
   render() {
     return (
-
       <div className="main-container">
         <div className="title"><a href="http://mobilespace.xyz/" className="main-link">MobileSpace</a> Community Members</div>
         <div className="landing-page-avatar-container">
@@ -32,6 +31,18 @@ export default class App extends Component {
           </Link>
           <Link to="/monte/locked">
             <img className="landing-page-avatar" src={MONTE_AVATAR} />
+          </Link>
+          <Link to="/emanuel/locked">
+            <img className="landing-page-avatar" src={EMANUEL_AVATAR} />
+          </Link>
+          <Link to="/girish">
+            <img className="landing-page-avatar" src={GIRISH_AVATAR} />
+          </Link>
+          <Link to="/nicholas/locked">
+            <img className="landing-page-avatar" src={NICHOLAS_AVATAR} />
+          </Link>
+          <Link to="/thomas/locked">
+            <img className="landing-page-avatar" src={THOMAS_AVATAR} />
           </Link>
           <Link to="/mitul/locked">
             <img className="landing-page-avatar" src={MITUL_AVATAR} />
@@ -42,17 +53,11 @@ export default class App extends Component {
           <Link to="/teodora/locked">
             <img className="landing-page-avatar" src={TEODORA_AVATAR} />
           </Link>
+          <Link to="/ryan/locked">
+            <img className="landing-page-avatar" src={RYAN_AVATAR} />
+          </Link>
           <Link to="/prakash/locked">
             <img className="landing-page-avatar" src={PRAKASH_AVATAR} />
-          </Link>
-          <Link to="/emanuel/locked">
-            <img className="landing-page-avatar" src={EMANUEL_AVATAR} />
-          </Link>
-          <Link to="/affaan">
-            <img className="landing-page-avatar" src={AFFAAN_AVATAR} />
-          </Link>
-          <Link to="/nicholas/locked">
-            <img className="landing-page-avatar" src={NICHOLAS_AVATAR} />
           </Link>
           <Link to="/jakhongir/locked">
             <img className="landing-page-avatar" src={JAKHONGIR_AVATAR} />
@@ -60,17 +65,8 @@ export default class App extends Component {
           <Link to="/sukhjit/locked">
             <img className="landing-page-avatar" src={SUKHJIT_AVATAR} />
           </Link>
-          <Link to="/mitul">
-            <img className="landing-page-avatar" src={MITUL_AVATAR} />
-          </Link>
-          <Link to="/ryan/locked">
-            <img className="landing-page-avatar" src={RYAN_AVATAR} />
-          </Link>
-          <Link to="/thomas/locked">
-            <img className="landing-page-avatar" src={THOMAS_AVATAR} />
-          </Link>
-          <Link to="/girish">
-            <img className="landing-page-avatar" src={GIRISH_AVATAR} />
+          <Link to="/affaan">
+            <img className="landing-page-avatar" src={AFFAAN_AVATAR} />
           </Link>
           <Link to="/karan">
             <img className="landing-page-avatar" src={KARAN_AVATAR} />
